8a4bf1b4b3851f84e7420c166f363a9c65189675
seoyeon-double-7 <s2103@e-mirim.hs.kr>
2023-06-11 13:41:13 +0900

    장애물 추가

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { Player } from "./player.js";
 import { InputHandler } from "./input.js";
 import { Background } from "./background.js";
+import { FlyingEnemy, GroundEnemy, ClimbingEnemy } from "./enemies.js";
 
 window.addEventListener("load", function () {
   const canvas = document.getElementById("canvas1");
@@ -20,15 +21,42 @@ window.addEventListener("load", function () {
       this.background = new Background(this);
       this.player = new Player(this);
       this.input = new InputHandler();
+
+      // 적
+      this.enemies = [];
+      this.enemyTimer = 0;
+      this.enemyInterval = 1000;
     }
     update(deltaTime) {
       this.background.update();
       this.player.update(this.input.keys, deltaTime);
+      // handleEnemies
+      if (this.enemyTimer > this.enemyInterval) {
+        this.addEnemy();
+        this.enemyTimer = 0;
+      } else {
+        this.enemyTimer += deltaTime;
+      }
+      this.enemies.forEach((enemy) => {
+        enemy.update(deltaTime);
+        if (enemy.markedForDelition)
+          this.enemies.splice(this.enemies.indexOf(enemy), 1);
+      });
     }
 
     draw(context) {
       this.background.draw(context);
       this.player.draw(context);
+      this.enemies.forEach((enemy) => {
+        enemy.draw(context);
+      });
+    }
+    addEnemy() {
+      if (this.speed > 0 && Math.random() < 0.5)
+        this.enemies.push(new GroundEnemy(this));
+      else if (this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
+      this.enemies.push(new FlyingEnemy(this));
+      console.log(this.enemies);
     }
   }
 
